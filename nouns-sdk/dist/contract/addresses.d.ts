233caafe1b60254c0db68dc24f452b0bfabd820b
ahsenriaz <ahsenriaz2001@gmail.com>
2022-10-17 20:43:26 +0500

    Fixed typescript bugs

--- nouns-sdk/dist/contract/addresses.d.ts
+++
@@ -1,8 +0,0 @@
-import { ContractAddresses } from './types';
-/**
- * Get addresses of contracts that have been deployed to the
- * Ethereum mainnet or a supported testnet. Throws if there are
- * no known contracts deployed on the corresponding chain.
- * @param chainId The desired chainId
- */
-export declare const getContractAddressesForChainOrThrow: (chainId: number) => ContractAddresses;
