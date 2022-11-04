2d304d67f40b1847da0eab432c9e1a5b3e3b9470
ahsenriaz <ahsenriaz2001@gmail.com>
2022-11-05 01:58:06 +0500

    Update index.tsx

--- packages/nouns-webapp/src/components/Tooltip/index.tsx
+++ packages/nouns-webapp/src/components/Tooltip/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactTooltip from 'react-tooltip';
-import classes from './Tooltip.module.css';
+import React from "react";
+import ReactTooltip from "react-tooltip";
+import classes from "./Tooltip.module.css";
 
 interface TooltipProps {
   tooltipContent: (dataTip: string) => React.ReactNode;
@@ -8,7 +8,7 @@ interface TooltipProps {
   id: string;
 }
 
-const Tooltip: React.FC<TooltipProps> = props => {
+const Tooltip: React.FC<TooltipProps> = (props) => {
   const { tooltipContent, tip, id } = props;
 
   return (
@@ -16,7 +16,7 @@ const Tooltip: React.FC<TooltipProps> = props => {
       <ReactTooltip
         id={id}
         className={classes.hover}
-        getContent={dataTip => {
+        getContent={(dataTip: any) => {
           return tooltipContent(dataTip);
         }}
       />
